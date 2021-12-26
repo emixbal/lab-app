5a1e99756f0a293e0dd2a84f73d80b1be66e71af
Muhammad Iqbal <emixbal>
2021-12-26 18:10:58 +0700

    list image from minio

--- app/models/product.models.go
+++ app/models/product.models.go
@@ -1,10 +1,17 @@
 package models
 
 import (
+	"context"
 	"fmt"
 	"labqid/config"
+	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"time"
 
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"gorm.io/gorm"
 )
 
@@ -19,24 +26,78 @@ type Product struct {
 	User           User
 }
 
+type ProductResponse struct {
+	Name           string `json:"name" gorm:"index:idx_name,unique"`
+	Description    string `json:"description"`
+	Price          int16  `json:"price"`
+	UserId         int    `json:"user_id"`
+	UserName       string `json:"user_name"`
+	UserEmail      string `json:"user_email"`
+	ImageName      string `json:"image_name"`
+	ImageThumbName string `json:"image_thumb_name"`
+	ImageUrl       string `json:"image_url"`
+	ImageThumbUrl  string `json:"image_thumb_url"`
+}
+
 func FethAllProducts() (Response, error) {
-	var products []Product
+	var products_responses []ProductResponse
+	var products_response ProductResponse
 	var res Response
+
+	// ======
+	endpoint := os.Getenv("MINIO_ENDPOINT")
+	accessKeyID := os.Getenv("MINIO_ACEESS_ID")
+	secretAccessKey := os.Getenv("MINIO_SECRET_ACEESS_ID")
+	useSSL := false
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// =======
 
 	db := config.GetDBInstance()
 
-	if result := db.Find(&products); result.Error != nil {
+	rows, err_q := db.Table("products p").Select(
+		"p.name, p.description, p.price, u.id as user_id, u.name as user_name, u.email as user_email, p.image_name, p.image_thumb_name",
+	).Joins("left join users u on p.user_id = u.id").Rows()
+	if err_q != nil {
+		log.Println(err_q)
 		fmt.Print("error FethAllProducts")
-		fmt.Print(result.Error)
+		fmt.Print(err_q)
 
 		res.Status = http.StatusInternalServerError
 		res.Message = "error fetchin records"
-		return res, result.Error
+		return res, err_q
+	}
+
+	for rows.Next() {
+		db.ScanRows(rows, &products_response)
+
+		// Set request parameters for content-disposition.
+		reqParams := make(url.Values)
+		// Generates a presigned url which expires in a day.
+		image_url, err := minioClient.PresignedGetObject(context.Background(), os.Getenv("MINIO_BUCKET_NAME"), products_response.ImageName, time.Second*24*60*60, reqParams)
+		if err != nil {
+			fmt.Println(err)
+		}
+		image_thumb_url, err_thumb := minioClient.PresignedGetObject(context.Background(), os.Getenv("MINIO_BUCKET_NAME"), products_response.ImageThumbName, time.Second*24*60*60, reqParams)
+		if err_thumb != nil {
+			fmt.Println(err_thumb)
+		}
+		products_response.ImageUrl = fmt.Sprintf("%v", image_url)
+		products_response.ImageThumbUrl = fmt.Sprintf("%v", image_thumb_url)
+
+		products_responses = append(products_responses, products_response)
 	}
 
 	res.Status = http.StatusOK
 	res.Message = "success"
-	res.Data = products
+	res.Data = products_responses
 
 	return res, nil
 }
